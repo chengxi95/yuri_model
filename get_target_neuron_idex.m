c15b315ee05f6030479abd31bd31214b131d9e26
chengxi95 <chengxiphysics@gmail.com>
2023-10-19 15:37:56 -0400

    add plot for delay comparsion, also update the plot that has the best results so far, it shows the MD is mainly activated by pPFC instead of PFCD

---
+++ get_target_neuron_idex.m
@@ -0,0 +1,23 @@
+function neuron_index_list = get_target_neuron_id(firing_mat, base_start, base_end, test_start, test_end)
+    if nargin < 5
+        base_start = 100000;
+        base_end = 200000;
+        test_start = 200000;
+        test_end = 300000 - 1000;
+    end
+
+    baseline_firing_num = sum(firing_mat(:, base_start:base_end), 2);
+    test_firing_num = sum(firing_mat(:, test_start:test_end), 2);
+    
+    firing_ratio = test_firing_num./(baseline_firing_num + 1);
+
+    % find the bin with most number of neurons
+    [bin_count, bin_edges] = histcounts(firing_ratio);
+    [max_count, max_index] = max(bin_count);
+    bin_start = bin_edges(max_index);
+    bin_end = bin_edges(max_index+1);
+
+    % get the index for those neurons
+    neuron_index_list = find(firing_ratio > bin_start & firing_ratio < bin_end);3
+end
+
