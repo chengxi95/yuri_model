27a7381f77439f4a6a04aa899fa894432f4d78b6
chengxi95 <chengxiphysics@gmail.com>
2023-11-01 15:14:16 -0400

    add script to plot single trail

--- yuri_model_workingversion.m
+++ yuri_model_workingversion.m
@@ -1391,6 +1391,119 @@ plot(pPFC_firing_rate_timeseries, 'r'), xlim([st, final])
 legend('VA', 'MD', 'PFC remote')
 
 %%
+trial_num = 1;
+
+t_PFC_S_BT = zeros(numberofneurons, length(T));
+t_PFC_S_RC = zeros(numberofneurons, length(T));
+t_PFC_S_GC = zeros(numberofneurons, length(T));
+t_PFC_S_YT = zeros(numberofneurons, length(T));
+
+t_PFC_D_BT = zeros(numberofneurons, length(T));
+t_PFC_D_RC = zeros(numberofneurons, length(T));
+t_PFC_D_GC = zeros(numberofneurons, length(T));
+t_PFC_D_YT = zeros(numberofneurons, length(T));
+
+t_VA_shape = zeros(numberofneurons, length(T));
+t_VA_ori = zeros(numberofneurons, length(T));
+
+t_PFC_remote_shape = zeros(numberofneurons, length(T));
+t_PFC_remote_ori = zeros(numberofneurons, length(T));
+
+t_MD_shape = zeros(numberofneurons, length(T));
+t_MD_ori = zeros(numberofneurons, length(T));
+
+for i = 1:length(full_PFC_S_BT{trial_num})
+    t_PFC_S_BT(full_PFC_S_BT{trial_num}(i, 1), full_PFC_S_BT{trial_num}(i, 2)) = 1;
+end
+
+for i = 1:length(full_PFC_S_RC{trial_num})
+    t_PFC_S_RC(full_PFC_S_RC{trial_num}(i, 1), full_PFC_S_RC{trial_num}(i, 2)) = 1;
+end
+
+for i = 1:length(full_PFC_S_GC{trial_num})
+    t_PFC_S_GC(full_PFC_S_GC{trial_num}(i, 1), full_PFC_S_GC{trial_num}(i, 2)) = 1;
+end
+
+for i = 1:length(full_PFC_S_YT{trial_num})
+    t_PFC_S_YT(full_PFC_S_YT{trial_num}(i, 1), full_PFC_S_YT{trial_num}(i, 2)) = 1;
+end
+
+for i = 1:length(full_PFC_D_BT{trial_num})
+    t_PFC_D_BT(full_PFC_D_BT{trial_num}(i, 1), full_PFC_D_BT{trial_num}(i, 2)) = 1;
+end
+
+for i = 1:length(full_PFC_D_RC{trial_num})
+    t_PFC_D_RC(full_PFC_D_RC{trial_num}(i, 1), full_PFC_D_RC{trial_num}(i, 2)) = 1;
+end
+
+for i = 1:length(full_PFC_D_GC{trial_num})
+    t_PFC_D_GC(full_PFC_D_GC{trial_num}(i, 1), full_PFC_D_GC{trial_num}(i, 2)) = 1;
+end
+
+for i = 1:length(full_PFC_D_YT{trial_num})
+    t_PFC_D_YT(full_PFC_D_YT{trial_num}(i, 1), full_PFC_D_YT{trial_num}(i, 2)) = 1;
+end
+
+for i = 1:length(full_VA_shape{trial_num})
+    t_VA_shape(full_VA_shape{trial_num}(i, 1), full_VA_shape{trial_num}(i, 2)) = 1;
+end
+
+for i = 1:length(full_PFC_remote_shape{trial_num})
+    t_PFC_remote_shape(full_PFC_remote_shape{trial_num}(i, 1), full_PFC_remote_shape{trial_num}(i, 2)) = 1;
+end
+
+for i = 1:length(full_MD_shape{trial_num})
+    t_MD_shape(full_MD_shape{trial_num}(i, 1), full_MD_shape{trial_num}(i, 2)) = 1;
+end
+
+figure(1)
+
+subplot(4,1,1)
+spy( t_PFC_S_BT,8,'b'),title('PFC Superficial blue triangle', 'FontSize', 16)
+set(gca,'DataAspectRatio',[1000 1 1]),ylabel('Neuron ID', 'FontSize',16), xlim([0 t_final/dt]);%,xlabel('time')
+
+
+subplot(4,1,2)
+spy( t_PFC_S_RC,8,'b'),title('PFC Superficial red circle ', 'FontSize', 16)
+set(gca,'DataAspectRatio',[1000 1 1]),ylabel('Neuron ID', 'FontSize',16), xlim([0 t_final/dt]);%,xlabel('time')
+
+subplot(4,1,3)
+spy( t_PFC_S_GC,8,'b'),title('PFC Superficial green circle ', 'FontSize', 16)
+set(gca,'DataAspectRatio',[1000 1 1]),ylabel('Neuron ID', 'FontSize',16), xlim([0 t_final/dt]);%,xlabel('time')
+
+subplot(4,1,4)
+spy(t_PFC_S_YT,8,'b'),title('PFC Superficial yellow triangle', 'FontSize', 16)
+set(gca,'DataAspectRatio',[1000 1 1]),ylabel('Neuron ID', 'FontSize',16), xlim([0 t_final/dt]);%,xlabel('time')
+%
+figure (3)
+subplot(4,1,1)
+spy( t_PFC_D_BT,8,'k'),title('PFC Deep blue triangle', 'FontSize', 16)
+set(gca,'DataAspectRatio',[1000 1 1]),ylabel('Neuron ID', 'FontSize',16), xlim([0 t_final/dt]);%,xlabel('time')
+subplot(4,1,2)
+spy( t_PFC_D_RC,8,'k'),title('PFC Deep red circle', 'FontSize', 16)
+set(gca,'DataAspectRatio',[1000 1 1]),ylabel('Neuron ID', 'FontSize',16), xlim([0 t_final/dt]);%,xlabel('time')
+
+subplot(4,1,3)
+spy( t_PFC_D_GC,8,'k'),title('PFC Deep green circle', 'FontSize', 16)
+set(gca,'DataAspectRatio',[1000 1 1]),ylabel('Neuron ID', 'FontSize',16), xlim([0 t_final/dt]);%,xlabel('time')
+subplot(4,1,4)
+spy( t_PFC_D_YT,8,'k'),title('PFC Deep yellow triangle', 'FontSize', 16)
+set(gca,'DataAspectRatio',[1000 1 1]),ylabel('Neuron ID', 'FontSize',16), xlim([0 t_final/dt]);%,xlabel('time')
+
+
+figure( 2)
+subplot(3,1,1)
+spy( t_VA_shape,8,'k'),title('VA Thalamus Shape', 'FontSize', 16)
+set(gca,'DataAspectRatio',[1000 1 1]),ylabel('Neuron ID', 'FontSize',16), xlim([0 t_final/dt]);%,xlabel('time')
+subplot(3,1,2)
+spy( t_PFC_remote_shape,8,'b'),title('remote PFC Shape', 'FontSize', 16)
+set(gca,'DataAspectRatio',[1000 1 1]),ylabel('Neuron ID', 'FontSize',16), xlim([0 t_final/dt]);%,xlabel('time')
+subplot(3,1,3)
+spy( t_MD_shape,8,'k'),title('MD', 'FontSize', 16)
+set(gca,'DataAspectRatio',[1000 1 1]),ylabel('Neuron ID', 'FontSize',16), xlim([0 t_final/dt]);%,xlabel('time')
+
+
+%%
 figure(1)
 
 subplot(4,1,1)
