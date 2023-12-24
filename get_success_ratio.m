3d6679b2cf7745ceefe720e13b52a73a5aa14261
Xi Cheng <chengxiphysics@gmail.com>
2023-12-23 23:17:33 -0500

    fix script for success ratio plotting

--- get_success_ratio.m
+++ get_success_ratio.m
@@ -9,7 +9,7 @@ function success_ratio = get_success_ratio(full_PFC, minimum_cell_num, minimum_p
         window_size = 100;
         minimum_cell_num = 20;
         minimum_peak_count = 4;
-        trial_num = 1;
+        trial_num = 200;
     end
     success_count = 0;
     total_count = 0;
@@ -19,7 +19,7 @@ function success_ratio = get_success_ratio(full_PFC, minimum_cell_num, minimum_p
             firing_matrix = zeros(numberofneurons, full_end);
             temp = full_PFC{i};
  
-            indexs = sub2ind(firing_matrix, temp(:, 1), temp(:, 2));
+            indexs = sub2ind(size(firing_matrix), temp(:, 1), temp(:, 2));
             firing_matrix(indexs) = firing_matrix(indexs) + 1;
             full_firing_num = sum(firing_matrix, 1);
             valid_peak_count = get_valid_peak_num(full_firing_num(base_start:base_end), full_firing_num(test_start:test_end), window_size, minimum_cell_num);
@@ -27,6 +27,7 @@ function success_ratio = get_success_ratio(full_PFC, minimum_cell_num, minimum_p
             if valid_peak_count >= minimum_peak_count
                 success_count = success_count + 1;
             end
+           
             total_count = total_count + 1;
         end
     end
--- get_valid_peak_num.m
+++ get_valid_peak_num.m
@@ -1,10 +1,12 @@
 function valid_peak_num = get_valid_peak_num(baseline_firing, test_firing, window_size, minimum_cell_num)
     baseline_sum = movsum(baseline_firing, window_size);
     test_sum = movsum(test_firing, window_size);
+    
+    baseline_sum(baseline_sum < minimum_cell_num) = 0;
+    test_sum(test_sum < minimum_cell_num) = 0;
 
-    baseline_peak_num = length(findpeaks(baseline_sum > minimum_cell_num));
-    test_peak_num = length(findpeaks(test_sum > minimum_cell_num));
+    
+    baseline_peak_num = length(findpeaks(baseline_sum));
+    test_peak_num = length(findpeaks(test_sum));
     valid_peak_num = test_peak_num - baseline_peak_num;
-
-    plot(baseline_sum)
 end
